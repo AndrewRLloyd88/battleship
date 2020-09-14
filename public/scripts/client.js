5fa2c5ae3eb6126cb3bc21b7a29426890ac7f62c
Andrew Lloyd <andrew.lloyd01@googlemail.com>
2020-09-13 18:06:46 -0700

    added functionality in to distinguish between ship sizes and to highlight endpoints on board during placement state

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,85 +1,227 @@
 $(document).ready(() => {
 
-//grabbing each clickable square
-const $addShip = $('.addShip');
-//grab our ship selection form - this displays all ships that are left to place
-let $currentShipsToPlace = $("#ship-choice")
-//this is grabbing the val of our form e.g. Carrier
-let $currentSelectedShip = $currentShipsToPlace.val().toLowerCase()
-//define the benchmark for ship sizes
-const carrierSize = 5;
-const battleShipSize = 4;
-const cruiserSize = 3;
-const submarineSize = 3;
-const destroyerSize = 2;
+  //helper that defines the selected ships size
+  const defineShipSize = (shipSize, currentSelectedSize) => {
+    const shipSizes = {
+      carrierSize: 5,
+      battleshipSize: 4,
+      cruiserSize: 3,
+      submarineSize: 3,
+      destroyerSize: 2
+    }
 
-//define the remaining placements for ships
-let remCarrierSize = 5;
-let remBattleShipSize = 4;
-let remCruiserSize = 3;
-let remSubmarineSize = 3;
-let remDestroyerSize = 2;
-
-//defining and holding the players ship position //how do we send this over to another player/AI?
-const carrierPos = [];
-const battleShipPos = [];
-const cruiserPos = [];
-const submarinePos = [];
-const destroyerPos = [];
+    for (const key in shipSizes) {
+      // console.log(shipSizes[key])
+      // console.log(currentSelectedSize, " current selected size")
+      // console.log(key, " current key")
+      if (currentSelectedSize === key) {
+        // console.log("true");
+        shipSize = shipSizes[key];
+        // console.log(shipSize)
+        return shipSize;
+      }
+    }
+  }
 
 
-//READY PHASE - The player will pick where their 5 ships need to be on the board
+  //grabbing each clickable square
+  const $addShip = $('.addShip');
+  //grab our ship selection form - this displays all ships that are left to place
+  let $currentShipsToPlace = $("#ship-choice")
+  //this is grabbing the val of our form e.g. Carrier
+  let $currentSelectedShip = $currentShipsToPlace.val().toLowerCase()
 
-//we need to be listening for what ship the player wants to pick
-//we need an event listener on our form!
-//we need to grab the val from the selection form data
-
-
-// let $currentShipToPlace = $("#ship-choice").val()
-// console.log($currentShipToPlace);
-
-// I am going to do the player selection based on Starting Point, Ending Point
-
-//we need an event listener for our form too
-//testin
-console.log($currentSelectedShip + "outside of listener")
-
-$currentShipsToPlace.change(function(){
-  // console.log($(this).val())
-  $currentSelectedShip = $(this).val().toLowerCase()
-  console.log($currentSelectedShip)
-})
+  //define the remaining ship size by doing stored remCarrierSize - carrierSize?
+  let currentSelectedSize = `${$currentSelectedShip}Size`;
+  let shipSize = 0;
 
 
-$addShip.click(function(evt) {
-  console.log($currentSelectedShip)
-  $(this).addClass(`${$currentSelectedShip}`)
-})
+  shipSize = defineShipSize(shipSize, currentSelectedSize);
+  console.log(shipSize, " On Document Ready");
+
+  
+  //defining and holding the players ship position //how do we send this over to another player/AI?
+  const carrierPos = [];
+  const battleShipPos = [];
+  const cruiserPos = [];
+  const submarinePos = [];
+  const destroyerPos = [];
+
+  //we need to set a start and a potential end point
+  //A1 or 0,0 would translate to 0,5 / 5,0 for a carrier - 0,2, 2,0 for a destroyer
+  let startPoint = "";
+  // let endPoints = [];
+
+  //we need a boolean to track our states
+  let isPlacingShip = false;
 
 
-// I am going to do the player selection based on Starting Point, Ending Point
-// 11:43
-// so the UI will go to player Pick a starting point (for your selected ship)
-// 11:44
-// so if its carrier say you place it on square A1, my reference #ID for that square in P1 board will be 0,0
-// 11:44
-// so then the logic will go based on size of selected ship potential endpoints will be current co-ordinates
-// 11:44
-// check if its edge of the board!?
-// 11:45
-// and then do + 0, SizeOfShip and SizeofShip, 0
-// 11:45
-// so that should for a carrier translate into like 0,5
-// 11:45
-// or 5,0
-// 11:46
-// and then logic (if a player does not click on 0,5 || 5,0 then return error (you have not selected a valid endpoint) else { ship === placed}
-// 11:47
-// and then store all the coordinates in an array so a carrier could look like = [0,0, 0,1, 0,2, 0,3, 04] (psuedocode)
-// 11:47
-// and so in the "play" phase we could say when player clicks one square go through each ship and check each array if the input class [0,0] is included in any array
-// 11:48
-// if its included 
+  //READY PHASE - The player will pick where their 5 ships need to be on the board
+
+  //listen for what ships the player wants to pick
+  $currentShipsToPlace.change(function () {
+    // console.log($(this).val())
+    //Any time a different ship is picked we need to update this from the form value
+    $currentSelectedShip = $(this).val().toLowerCase();
+    //we need to update it's selected size
+    currentSelectedSize = `${$currentSelectedShip}Size`;
+    // we need to update the ships size variable we will use to track our players positioning
+    shipSize = defineShipSize(shipSize, currentSelectedSize);
+    console.log(shipSize, " updated in form listener to be our " + $currentSelectedShip + " size");
+
+    //we need some logic to stop this from being selected upon a placement/bar illegal placements later
+  });
+
+
+  //listening for a player to click for any particular ship
+  $addShip.click(function (evt) {
+    // I am going to do the player selection based on Starting Point, Ending Point
+    //is the player placing a ship?
+    if (!isPlacingShip) {
+      console.log($currentSelectedShip)
+      //adds that colour to the square
+      $(this).addClass(`${$currentSelectedShip}`)
+      //make the starting position the current grid square by ID
+      startPoint = $(this).attr("id");
+      console.log(startPoint);
+      //we need to set the switch on to indicate we are placing a ship
+      isPlacingShip = true;
+      //we need to calculate end placements
+      const endPoints = calculateEndPlacements(startPoint, shipSize);
+      console.log("in click: ", endPoints)
+      renderEndPlacements(endPoints);
+    } else {
+      //calculate and display selected end placements for ships
+
+      placeShipTemplate();
+    }
+  });
+
+  // function that takes in the ship, the length, the id of the square etc?
+  const placeShipTemplate = () => {
+    //check where the starting coords are
+    for (i = 0; i < shipSize; i++) {
+      console.log("hello");
+    }
+
+  }
+
+  const calculateEndPlacements = (startPoint, shipSize, endPoints) => {
+    let defaultStartX = Number(startPoint[0]);
+    let defaultStartY = Number(startPoint[2]);
+
+    let startX = Number(startPoint[0]);
+    let startY = Number(startPoint[2]);
+
+    let negStartX = Number(startPoint[0]);
+    let negStartY = Number(startPoint[2]);
+
+    let newEndpoint = [];
+    shipSize = shipSize - 1
+    //we can base our end placements off the ship size the starting point and determine the end point
+    // console.log(typeof startPoint);
+    console.log(startX, startY);
+    console.log(shipSize)
+    //we need to look 4 directions to say where can the legal endpoints of the ship be?
+    // could we put all of these actions in a loop to lose the repetitive code?
+    //consider using helper functions and passing the values via these?
+
+
+    //check X+
+    if (defaultStartX + shipSize > 9) {
+      startX = null;
+    } else {
+      startX += shipSize;
+    }
+    //check Y+
+    if (defaultStartY + shipSize > 9) {
+      startY = null;
+    } else {
+      startY += shipSize;
+    }
+
+    console.log("defaultStartX - shipSize: =  ", defaultStartX - shipSize)
+    console.log("defaultStartX - shipSize: =  ", defaultStartX - shipSize < 0)
+    //check -X
+    if (defaultStartX - shipSize < 0) {
+      negStartX = null;
+    } else {
+      negStartX -= shipSize;
+      console.log(negStartX + " in else")
+    }
+    //check Y-
+    if (defaultStartY - shipSize < 0) {
+      negStartY = null;
+    } else {
+      negStartY -= shipSize;
+    }
+
+    console.log("+x: " + startX)
+    console.log("+y: " + startY)
+    console.log("-x: " + negStartX)
+    console.log("-y: " + negStartY)
+
+    //are we in the middle? is our X or -X axis Valid?
+    if (startX !== null) {
+      console.log("We are IN THE MIDDLE X is: " + startX, "Y is: " + defaultStartY);
+      newEndpoint.push(`${startX},${defaultStartY}`)
+    }
+
+    if (negStartX !== null) {
+      console.log("We are IN THE MIDDLE X is: " + negStartX, "Y is: " + defaultStartY);
+      newEndpoint.push(`${negStartX},${defaultStartY}`)
+    }
+
+    //are we in the middle? is our X or -X axis Valid?
+    if (startY !== null) {
+      console.log("We are IN THE MIDDLE X is: " + defaultStartX, "Y is: " + startY);
+      newEndpoint.push(`${defaultStartX},${startY}`)
+    }
+
+    if (negStartY !== null) {
+      console.log("We are IN THE MIDDLE X is: " + defaultStartX, "Y is: " + negStartY);
+      newEndpoint.push(`${defaultStartX},${negStartY}`)
+    }
+    console.log(newEndpoint);
+    return newEndpoint;
+  }
+
+
+  const renderEndPlacements = (endPoints) => {
+    console.log(endPoints)
+    for (coords of endPoints) {
+      // console.log(coords)
+      // console.log(`${coords}`)
+      // console.log(`#${coords[0]}\\,${coords[2]}`)
+      //we must use escape characters to use our co-ords
+      $(`#${coords[0]}\\,${coords[2]}`).addClass("highlight");
+    }
+  }
+
+
+  // I am going to do the player selection based on Starting Point, Ending Point
+  // 11:43
+  // so the UI will go to player Pick a starting point (for your selected ship)
+  // 11:44
+  // so if its carrier say you place it on square A1, my reference #ID for that square in P1 board will be 0,0
+  // 11:44
+  // so then the logic will go based on size of selected ship potential endpoints will be current co-ordinates
+  // 11:44
+  // check if its edge of the board!?
+  // 11:45
+  // and then do + 0, SizeOfShip and SizeofShip, 0
+  // 11:45
+  // so that should for a carrier translate into like 0,5
+  // 11:45
+  // or 5,0
+  // 11:46
+  // and then logic (if a player does not click on 0,5 || 5,0 then return error (you have not selected a valid endpoint) else { ship === placed}
+  // 11:47
+  // and then store all the coordinates in an array so a carrier could look like = [0,0, 0,1, 0,2, 0,3, 04] (psuedocode)
+  // 11:47
+  // and so in the "play" phase we could say when player clicks one square go through each ship and check each array if the input class [0,0] is included in any array
+  // 11:48
+  // if its included 
 
 
 
