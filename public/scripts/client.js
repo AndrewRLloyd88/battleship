1311ec9a9dc0e7122a8111d043cb15c424376c57
Andrew Lloyd <andrew.lloyd01@googlemail.com>
2020-09-15 22:18:42 -0700

    lots more code cleanup and added a lot of comments for clarity

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -15,13 +15,9 @@ $(document).ready(() => {
     };
 
     for (const key in shipSizes) {
-      // console.log(shipSizes[key])
-      // console.log(currentSelectedSize, " current selected size")
-      // console.log(key, " current key")
       if (currentSelectedSize === key) {
-        // console.log("true");
+        //this intially sets the value of our ships size
         shipSize = shipSizes[key];
-        // console.log(shipSize)
         return shipSize;
       }
     }
@@ -43,16 +39,19 @@ $(document).ready(() => {
   //grab our warning message section
   const $warning = $('#warning');
 
-  //define the remaining ship size by doing stored remCarrierSize - carrierSize?
+  //we are dyanmically setting a variable from our form e.g. carrierSize - this is checked in defineShipSize()
   let currentSelectedSize = `${$currentSelectedShip}Size`;
   let shipSize = 0;
 
+  //this is where we store our endpoints when player is placing the ship
   let endPoints = [];
+  //this keeps track of our selected endpoint - used to check player is not trying to misplace ships! 
+  //curEndPoint === evt.target.id
   let curEndpoint = "";
 
   //defining and holding the players ship position //how do we send this over to another player/AI?
   const playersShipsPos = {};
-  //has our player placed a class of ship?
+  //has our player placed a class of ship? - we keep track of this here
   const playersPlacedShips = {
     carrier: false,
     battleship: false,
@@ -65,9 +64,6 @@ $(document).ready(() => {
   shipSize = defineShipSize(shipSize, currentSelectedSize);
   // console.log(shipSize, " On Document Ready");
 
-
-
-
   //we need to set a start and a potential end point
   //A1 or 0,0 would translate to 0,5 / 5,0 for a carrier - 0,2, 2,0 for a destroyer
   let startPoint = "";
@@ -75,12 +71,13 @@ $(document).ready(() => {
   //we need a boolean to track our states
   let isPlacingShip = false;
 
+
+  //used when placing a ship need conditional logic here to check if ship has already been placed
   $warning.text("Please place your " + $currentSelectedShip + "'s starting position");
   //READY PHASE - The player will pick where their 5 ships need to be on the board
 
   //listen for what ships the player wants to pick
   $currentShipsToPlace.change(function () {
-    // console.log($(this).val())
     //Any time a different ship is picked we need to update this from the form value
     $currentSelectedShip = $(this).val().toLowerCase();
     //we need to update it's selected size
@@ -105,7 +102,6 @@ $(document).ready(() => {
     }
     //is the player placing a ship?
     if (!isPlacingShip) {
-      // console.log($currentSelectedShip)
       //make our form invisible
       $currentShipsToPlace.addClass('hidden');
       //change our message to user
@@ -115,15 +111,11 @@ $(document).ready(() => {
       //make the starting position the current grid square by ID
       startPoint = $(this).attr("id");
       // currentShipToChart.push(startPoint);
-      console.log(startPoint);
       playersShipsPos[$currentSelectedShip] = [startPoint];
-      console.log(playersShipsPos);
       //we need to set the switch on to indicate we are placing a ship
       isPlacingShip = true;
       //we need to calculate end placements
-      console.log("logging endpoints before: ", endPoints)
       endPoints = calculateEndPlacements(startPoint, shipSize, playersShipsPos);
-      console.log("I MUST LOG OUT THE ENDPOINTS: ", endPoints);
       // console.log("in click: ", endPoints)
       renderEndPlacements(endPoints);
       //show cancel button
@@ -133,19 +125,15 @@ $(document).ready(() => {
       //are we placing an endpoint?
     } else {
       //calculate and display selected end placements for ships
-      console.log("in else");
       //check the player has legally placed the piece
       if (checkLegalPlacement(endPoints, evt)) {
         //add the current endpoint to our array
         playersShipsPos[$currentSelectedShip].push(curEndpoint);
-        console.log(playersShipsPos);
         //make a function to fill in the dots dependent on size of ship
-        console.log(shipSize);
         if (shipSize !== 2) {
           //run our new function to fill in the dots
           addShipToArray($currentSelectedShip, playersShipsPos);
         }
-        console.log("past the if!");
         //remove rendering for ships
         $(this).removeClass(`${$currentSelectedShip}`);
         //remove endplacement template
@@ -175,46 +163,40 @@ $(document).ready(() => {
   };
 
   //calculates on our board legal placements for each ship
-  //we need to modify this function to take account of existing ships
   const calculateEndPlacements = (startPoint, shipSize, playersShipsPos) => {
+    //keeping track of our original X and Y position
     let defaultStartX = Number(startPoint[0]);
     let defaultStartY = Number(startPoint[2]);
-
+    //test these for +X and Y coords
     let startX = Number(startPoint[0]);
     let startY = Number(startPoint[2]);
-
+    //test these for -X and -Y coords
     let negStartX = Number(startPoint[0]);
     let negStartY = Number(startPoint[2]);
-
+    //this will hold our basic endpoints before checking for overlapping ships
     let newEndpoint = [];
+    //we don't need to include our start position so ship size - 1
     shipSize = shipSize - 1;
 
-
     //we can base our end placements off the ship size the starting point and determine the end point
-    // console.log(typeof startPoint);
-    // console.log(startX, startY);
-    // console.log(shipSize)
-    //we need to look 4 directions to say where can the legal endpoints of the ship be?
+    //we need to look in 4 directions to say where can the legal endpoints of the ship be?
     // could we put all of these actions in a loop to lose the repetitive code?
     //consider using helper functions and passing the values via these?
 
-
-    //check X+
+    //check +X
     if (defaultStartX + shipSize > 9) {
       startX = null;
       //we need a new function to check intersections
     } else {
       startX += shipSize;
     }
-    //check Y+
+    //check +Y
     if (defaultStartY + shipSize > 9) {
       startY = null;
     } else {
       startY += shipSize;
     }
 
-    // console.log("defaultStartX - shipSize: =  ", defaultStartX - shipSize)
-    // console.log("defaultStartX - shipSize: =  ", defaultStartX - shipSize < 0)
     //check -X
     if (defaultStartX - shipSize < 0) {
       negStartX = null;
@@ -229,50 +211,33 @@ $(document).ready(() => {
       negStartY -= shipSize;
     }
 
-    // console.log("+x: " + startX)
-    // console.log("+y: " + startY)
-    // console.log("-x: " + negStartX)
-    // console.log("-y: " + negStartY)
-
     //are we in the middle? is our X or -X axis Valid?
     if (startX !== null) {
-      // console.log("We are IN THE MIDDLE X is: " + startX, "Y is: " + defaultStartY);
       newEndpoint.push(`${startX},${defaultStartY}`);
     }
 
     if (negStartX !== null) {
-      // console.log("We are IN THE MIDDLE X is: " + negStartX, "Y is: " + defaultStartY);
       newEndpoint.push(`${negStartX},${defaultStartY}`);
     }
 
     //are we in the middle? is our X or -X axis Valid?
     if (startY !== null) {
-      // console.log("We are IN THE MIDDLE X is: " + defaultStartX, "Y is: " + startY);
       newEndpoint.push(`${defaultStartX},${startY}`);
     }
 
     if (negStartY !== null) {
-      // console.log("We are IN THE MIDDLE X is: " + defaultStartX, "Y is: " + negStartY);
       newEndpoint.push(`${defaultStartX},${negStartY}`);
     }
-    // console.log(newEndpoint);
+
 
     //will the selected place intersect with another ship
     return checkEndsNotIntersect(newEndpoint, playersShipsPos);
-    // console.log(newEndpoint)
-
-
-    // return newEndpoint;
   };
 
   //renders the highlights on the board showing where the legal endpoints are
   const renderEndPlacements = (endPoints) => {
-    // console.log(endPoints)
     for (const coords of endPoints) {
-      // console.log(coords)
-      // console.log(`${coords}`)
-      // console.log(`#${coords[0]}\\,${coords[2]}`)
-      //we must use escape characters to use our co-ords
+      //draw suggested placements on screen for player (yellowgreen)
       $(`#${coords[0]}\\,${coords[2]}`).addClass("highlight");
     }
   };
@@ -298,7 +263,6 @@ $(document).ready(() => {
     } else {
       //we work out the logic to draw the ship
       curEndpoint = evt.target.id;
-      console.log("Good Boi");
       $warning.text("");
       //we need to return a truthy value
       return true;
@@ -307,19 +271,17 @@ $(document).ready(() => {
 
   //fills in the dots for the players Ships using the start and endpoints
   const addShipToArray = ($currentSelectedShip, playersShipsPos) => {
-    const arr = [];
+    //we are using startPos and endPos to render our ships path e.g. startpos endpost for $cruiser
     const [startPos, endPos] = (playersShipsPos[`${$currentSelectedShip}`]);
-    console.log(startPos, endPos);
-
 
     //is 4,3 < 0,3 //CHECKING NEGATIVE X ?
     //the player has placed right to left
     if (startPos[0] > endPos[0]) {
-      console.log("minX is more than maxX");
       //will write out such output to the arr as [3,3, 2,3, 1,3, 0,3]
       for (let i = startPos[0]; i >= endPos[0]; i--) {
-        console.log(i);
+        //this pushes the positions into the array if they aren't already there
         if (!playersShipsPos[`${$currentSelectedShip}`].includes(`${i},${startPos[2]}`)) {
+          //push the dynamic position into the array in playerShipsPos object
           playersShipsPos[`${$currentSelectedShip}`].push(`${i},${startPos[2]}`);
         }
       }
@@ -331,7 +293,6 @@ $(document).ready(() => {
     //is 4,3 < 0,3 //CHECKING NEGATIVE X ?
     //the player has placed left to right
     if (endPos[0] > startPos[0]) {
-      console.log("endpos[0] is greater than startpos[0]");
       //will write out such output to the arr as [3,3, 2,3, 1,3, 0,3]
       for (let i = startPos[0]; i < endPos[0]; i++) {
         if (!playersShipsPos[`${$currentSelectedShip}`].includes(`${i},${startPos[2]}`)) {
@@ -360,11 +321,12 @@ $(document).ready(() => {
     if (startPos[2] > endPos[2]) {
       //will write out such output to the arr as [3,3, 2,3, 1,3, 0,3]
       for (let i = endPos[2]; i < startPos[2]; i++) {
-        //does any of our coordina
+        //if 
         if (!playersShipsPos[`${$currentSelectedShip}`].includes(`${startPos[0]},${i}`)) {
           playersShipsPos[`${$currentSelectedShip}`].push(`${startPos[0]},${i}`);
         }
       }
+      //sort the array in order
       playersShipsPos[`${$currentSelectedShip}`].sort();
       return playersShipsPos;
     }
@@ -380,68 +342,67 @@ $(document).ready(() => {
       //get our current ships placements to test against
       existingPlacements.push(...playersShipsPos[ships]);
     }
-    //used to count back to our starting pos
+    //used to count back from the furthest position away to our starting pos
     const currentPos = existingPlacements[existingPlacements.length - 1]
 
+    //this is needed to ensure the first ship is placed OK we need to go a different path for every other ship
+    if (existingPlacements.length > 1) {
+      //check all +/-X and +/-Y directions against existing ships to see if there is any intersection
+      for (const coords of newEndpoint) {
+        const x = coords[0]
+        const y = coords[2]
+        //check UP from BOTTOM of Board if a suggested path would clip our ships
+        if (y > currentPos[2]) {
+          for (let i = y; i > currentPos[2]; i--) {
+            //does the countback check
+            if (existingPlacements.includes(`${currentPos[0]},${i}`)) {
+              //push the banned points to coords
+              bannedPoints.push(coords);
 
+            }
+          }
+        }
 
-    //this is needed to ensure the first ship is placed OK
-    if(existingPlacements.length > 1) {
+        //Check DOWN from TOP OF BOARD if a path would clip our existing ships
+        if (y < currentPos[2]) {
+          for (let i = y; i < currentPos[2]; i++) {
+            //does the countback check
+            if (existingPlacements.includes(`${currentPos[0]},${i}`)) {
+              bannedPoints.push(coords)
 
-    for (const coords of newEndpoint) {
-      const x = coords[0]
-      const y = coords[2]
-      //check UP from BOTTOM of Board if a suggested path would clip our ships
-      if (y > currentPos[2]) {
-        for (let i = y; i > currentPos[2]; i--) {
-          //does the countback check
-          if (existingPlacements.includes(`${currentPos[0]},${i}`)) {
-            //push the banned points to coords
-            bannedPoints.push(coords);
-         
+            }
+          }
+        }
+
+        //Check Right to Left
+        if (x > currentPos[0]) {
+          const index = newEndpoint.indexOf(coords)
+          //Check RIGHT to LEFT OF BOARD if a path would clip our existing ships
+          for (let i = x; i > currentPos[0]; i--) {
+            //does the countback check
+            if (existingPlacements.includes(`${i},${currentPos[2]}`)) {
+              bannedPoints.push(coords);
+
+            }
+          }
+        }
+
+        //Check Left
+        if (x < currentPos[0]) {
+          const index = newEndpoint.indexOf(coords)
+          //Check RIGHT to LEFT OF BOARD if a path would clip our existing ships
+          for (let i = x; i < currentPos[0]; i++) {
+            //does the countback check
+            if (existingPlacements.includes(`${i},${currentPos[2]}`)) {
+              bannedPoints.push(coords);
+            }
           }
         }
       }
-
-      //Check DOWN from TOP OF BOARD if a path would clip our existing ships
-      if (y < currentPos[2]) {
-        for (let i = y; i < currentPos[2]; i++) {
-          //does the countback check
-          if (existingPlacements.includes(`${currentPos[0]},${i}`)) {
-            bannedPoints.push(coords)
-
-          }
-        }
-      }
-
-      //Check Right to Left
-      if (x > currentPos[0]) {
-        const index = newEndpoint.indexOf(coords)
-        //Check RIGHT to LEFT OF BOARD if a path would clip our existing ships
-        for (let i = x; i > currentPos[0]; i--) {
-          //does the countback check
-          if (existingPlacements.includes(`${i},${currentPos[2]}`)) {
-            bannedPoints.push(coords);
-
-          }
-        }
-      }
-    
-      //Check Left
-      if (x < currentPos[0]) {
-        const index = newEndpoint.indexOf(coords)
-        //Check RIGHT to LEFT OF BOARD if a path would clip our existing ships
-        for (let i = x; i < currentPos[0]; i++) {
-          //does the countback check
-          if (existingPlacements.includes(`${i},${currentPos[2]}`)) {
-            bannedPoints.push(coords);
-          }
-        }
-      }
+      //will end up generating legal endpoints that does not cause ships to overlap
+      newEndPoints = newEndpoint.filter((el) => !bannedPoints.includes(el));
+      return newEndPoints;
     }
-    newEndPoints = newEndpoint.filter( ( el ) => !bannedPoints.includes( el ) );
-    return newEndPoints;
-  }
     return newEndpoint;
   }
 
